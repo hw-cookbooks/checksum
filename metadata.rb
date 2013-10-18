8bd3c20103b6b9803a43120fe976c2fe23d52918
Sean Escriva <sean.escriva@gmail.com>
2013-10-18 15:47:36 -0700

    Add algorithm parameter

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name 'checksum'
 maintainer 'Heavy Water Ops, LLC'
-maintainer_email 'support@hw-ops.com'
+nmaintainer_email 'support@hw-ops.com'
 license 'Apache 2.0'
 description 'Library cookbook for simple checksum operations'
 
-version '0.1.0'
+version '0.1.1'
 
